68ee7a00c81339b62f2d2517114acc9a46f0be15
ryu1kup <ryu1kup@gmail.com>
2020-11-24 11:10:39 +0900

    iterable traits classes

---
+++ README.md
@@ -0,0 +1,30 @@
+# iterable_traits
+
+simple header-only library providing iterable traits classes.
+
+currently the following STL containers are supported:
+
+- `std::vector`
+- `std::list`
+- `std::forward_list`
+- `std::deque`
+- `std::set`
+- `std::multiset`
+- `std::unordered_set`
+- `std::unordered_multiset`
+
+# usave
+
+```
+#include "iterable_traits.h"
+#include <vector>
+
+int main(){
+    std::vector<int> v {1, 2, 3, 4, 5};
+
+    static_assert(detail::is_iterable<decltype(v)>::value);
+    static_assert(detail::is_iterable_v<decltype(v)>);
+    static_assert(detail::is_vector<decltype(v)>::value);
+    static_assert(detail::is_vector_v<decltype(v)>);
+}
+```
---
+++ iterable_traits.h
@@ -0,0 +1,106 @@
+#ifndef CPP_INTERNAL_DETAILS_ITERABLE_TRAITS
+#define CPP_INTERNAL_DETAILS_ITERABLE_TRAITS 1
+
+#include <iterator>
+#include <type_traits>
+#include <vector>
+#include <list>
+#include <forward_list>
+#include <deque>
+#include <set>
+#include <unordered_set>
+
+namespace detail {
+    // meta function for checking if the template argument is iterable or not
+    // here iterable means the template argument can be applied std::begin() and std::end()
+    template <class T, typename = void>
+    struct is_iterable : std::false_type {};
+
+    template <class T>
+    struct is_iterable<T, std::void_t<decltype(std::begin(std::declval<T>())), decltype(std::end(std::declval<T>()))>> : std::true_type {};
+
+    template <class T>
+    constexpr bool is_iterable_v = is_iterable<T>::value;
+
+    // meta function for checking if the template argument is std::vector<T>
+    template <class T>
+    struct is_vector : std::false_type {};
+
+    template <class T>
+    struct is_vector<std::vector<T>> : std::true_type {};
+
+    template <class T>
+    constexpr bool is_vector_v = is_vector<T>::value;
+
+    // meta function for checking if the template argument is std::list<T>
+    template <class T>
+    struct is_list : std::false_type {};
+
+    template <class T>
+    struct is_list<std::list<T>> : std::true_type {};
+
+    template <class T>
+    constexpr bool is_list_v = is_list<T>::value;
+
+    // meta function for checking if the template argument is std::forward_list<T>
+    template <class T>
+    struct is_forward_list : std::false_type {};
+
+    template <class T>
+    struct is_forward_list<std::forward_list<T>> : std::true_type {};
+
+    template <class T>
+    constexpr bool is_forward_list_v = is_forward_list<T>::value;
+
+    // meta function for checking if the template argument is std::deque<T>
+    template <class T>
+    struct is_deque : std::false_type {};
+
+    template <class T>
+    struct is_deque<std::deque<T>> : std::true_type {};
+
+    template <class T>
+    constexpr bool is_deque_v = is_deque<T>::value;
+
+    // meta function for checking if the template argument is std::set<T>
+    template <class T>
+    struct is_set : std::false_type {};
+
+    template <class T>
+    struct is_set<std::set<T>> : std::true_type {};
+
+    template <class T>
+    constexpr bool is_set_v = is_set<T>::value;
+
+    // meta function for checking if the template argument is std::multiset<T>
+    template <class T>
+    struct is_multiset : std::false_type {};
+
+    template <class T>
+    struct is_multiset<std::multiset<T>> : std::true_type {};
+
+    template <class T>
+    constexpr bool is_multiset_v = is_multiset<T>::value;
+
+    // meta function for checking if the template argument is std::unordered_set<T>
+    template <class T>
+    struct is_unordered_set : std::false_type {};
+
+    template <class T>
+    struct is_unordered_set<std::unordered_set<T>> : std::true_type {};
+
+    template <class T>
+    constexpr bool is_unordered_set_v = is_unordered_set<T>::value;
+
+    // meta function for checking if the template argument is std::unordered_multiset<T>
+    template <class T>
+    struct is_unordered_multiset : std::false_type {};
+
+    template <class T>
+    struct is_unordered_multiset<std::unordered_multiset<T>> : std::true_type {};
+
+    template <class T>
+    constexpr bool is_unordered_multiset_v = is_unordered_multiset<T>::value;
+}
+
+#endif
